d8d8966db36bc96a6f1d1db3106ea9fc8f456a3e
Arthur Conti <arthurgallotticonti@gmail.com>
2021-12-07 23:31:22 -0300

    final update'

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("db_host","localhost");
-define("db_port",3306);
-define("db_user","root");
-define("db_pass","");
-define("db_database","progweb");
-?>
--- php/model/connection.php
+++ php/model/connection.php
@@ -1,5 +1,9 @@
 <?php
-require_once("../config/config.php");
+define("db_host", "localhost");
+define("db_port", 3306);
+define("db_user", "root");
+define("db_pass", "");
+define("db_database", "progweb");
 class Connection
 {
     private static $DB_URL = db_host;
